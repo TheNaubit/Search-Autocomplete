77594c9c89fa97f689e20045f1ac2b3dd8739cb3
Alberto Del Águila <nautadev@gmail.com>
2020-01-26 22:41:48 +0100

    Added demo

---
+++ build/precache-manifest.3d750b3e2b5082064ae644b2f828b40d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb30857a00ac0106bf16e5508b56c5e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3208d1cb8b4aa596068",
+    "url": "/static/css/main.1c9b6159.chunk.css"
+  },
+  {
+    "revision": "a4fe83c36c26364ce9a3",
+    "url": "/static/js/2.d3ebcdf3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d3ebcdf3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a3208d1cb8b4aa596068",
+    "url": "/static/js/main.22547a56.chunk.js"
+  },
+  {
+    "revision": "e6ad427a885f0889d217",
+    "url": "/static/js/runtime-main.fa194ab5.js"
+  }
+]);
